cfeecac9fc2d2b15b443f101dfc99b9fe79a3308
mitranandanm <mitranandanm@gmail.com>
2020-09-13 21:24:42 +0530

    jenkins commit

---
+++ src/main/java/jenkins/jenkinsmaven/Arjun.java
@@ -0,0 +1,7 @@
+package jenkins.jenkinsmaven;
+
+public class Arjun {
+public static void main(String[] args) {
+	System.out.println("arjun asked me");
+}
+}
---
+++ src/main/java/jenkins/jenkinsmaven/Chamu.java
@@ -0,0 +1,5 @@
+package jenkins.jenkinsmaven;
+
+public class Chamu {
+
+}
